0898c8a6f45f28bb26de42177f2daa342abeeef8
jorge uilian <adelson.art@hotmail.com>
2019-04-30 10:57:34 -0300

    subindo o desbloqueio do servidor rubens laranjeiras malheiros

--- SistemaDiarias/Ajax/AjaxRegraBloqueio.php
+++ SistemaDiarias/Ajax/AjaxRegraBloqueio.php
@@ -254,7 +254,7 @@ if($qtdSoma >= 180)
 
 if($qtdSomaMes >= 15)
 {
-    if($Beneficiario == 5894 || $Beneficiario == 2219 || $Beneficiario == 1247 || $Beneficiario ==1841 || $Beneficiario ==3122){
+    if($Beneficiario == 5894 || $Beneficiario == 2219 || $Beneficiario == 1247 || $Beneficiario ==1841 || $Beneficiario ==3122 || $Beneficiario ==4985){
         if($qtdSomaMes >= 23){
             $PossuiBloqueio = 1;
 
